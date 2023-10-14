aa67718c6fb3e6321b29a79a253ff5fdd74b0873
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-10-14 11:15:55 -0500

    Socket COMPLETED

--- api-websocket/server.js
+++ api-websocket/server.js
@@ -9,10 +9,17 @@ const config = require('./config')
 app.use('/', express.static('public'))
 
 io.on('connection', function(socket){
-    console.log('Nuevo cliente conectado')
+    console.log('Nuevo cliente conectado.')
     socket.emit('mensaje', 'Bienvenido')
 })
 
-app.listen(config.PORT, function() {
+let contador = 1
+
+setInterval(function(){
+    io.emit(`mensaje`, `Hola, saludos a todos --> ${contador}`)
+    contador++
+}, 3000)
+
+server.listen(config.PORT, function() {
     console.log(`La aplicacion esta escuchando en http://localhost:${config.PORT}`)
 })
