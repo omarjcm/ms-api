c9fb83c01f347cbf24c6fd86d2633e52a52e5bf2
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-10-20 19:47:43 -0500

    Creacion de la pagina index.html con dos componentes.

---
+++ movie_maker/frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <div className='topnav'>
+            <a href="/#">Movie Maker</a>
+            <a href="/#" className='add-button'>Add Movie</a>
+        </div>
+     );
+}
+ 
+export default Header;
